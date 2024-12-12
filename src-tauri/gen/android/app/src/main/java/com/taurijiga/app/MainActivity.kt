755832350c4d2157374471418ea763fb1b55cf3f
Шушаков Александр <kolyasashatwo@gmail.com>
2024-12-12 19:00:40 +0700

    first commit

---
+++ src-tauri/gen/android/app/src/main/java/com/taurijiga/app/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.taurijiga.app
+
+class MainActivity : TauriActivity()
---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,24 @@
+use tauri::Manager;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_webview_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_serialplugin::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
